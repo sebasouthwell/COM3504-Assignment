351ee85d2bd4085d5823bedec3b84cab6fa35f8c
sasouthwell1 <sasouthwell1@sheffield.ac.uk>
2024-03-03 01:20:37 +0000

    Added Extra HTML Meta

--- routes/sighting.js
+++ routes/sighting.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-    res.render('sighting', { title: 'Plant info',stylesheets: ["css/bootstrap.min.css","stylesheets/style.css"], javascripts: ["javascripts/locationManager.js"]});
+    res.render('sighting', { title: 'Planttrest: Plant Sighting Form',stylesheets: ["css/bootstrap.min.css","stylesheets/style.css"], javascripts: ["javascripts/locationManager.js"]});
 });
 
 module.exports = router;
