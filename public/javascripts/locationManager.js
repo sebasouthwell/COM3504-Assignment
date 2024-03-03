66c081d4d48af6305ab37ac92c7769127a3b5a91
sasouthwell1 <sasouthwell1@sheffield.ac.uk>
2024-03-03 00:18:37 +0000

    Added GetLocation Script

--- public/javascripts/locationManager.js
+++ public/javascripts/locationManager.js
@@ -1,12 +1,32 @@
-window.addEventListener('load', function() {
-  var location = document.getElementById('location');
-  if (location) {
-    location.addEventListener('change', function() {
-      if (location.value) {
-        window.location = '/location/' + location.value;
-      } else {
-        window.location = '/';
-      }
-    });
+window.addEventListener('load', geolocationSupported);
+function geolocationSupported() {
+  let locButton = document.getElementById('get_location')
+  if (navigator.geolocation) {
+    console.log("Geolocation is supported by this browser :)");
+    locButton.classList.remove('hidden');
+  } else {
+    console.log("Geolocation is NOT supported by this browser :(");
   }
+}
+
+
+function getLocation(lat_class, long_class) {
+  let locButton = document.getElementById('get_location')
+  if (navigator.geolocation) {
+    locButton.classList.add('hidden');
+    navigator.geolocation.getCurrentPosition(function(result) {
+      document.getElementsByClassName(lat_class)[0].value = result.coords.latitude;;
+      document.getElementsByClassName(long_class)[0].value = result.coords.longitude;;
+    });
+    locButton.classList.remove('hidden');
+  } else {
+    console.log("Geolocation is not supported by this browser.");
+  }
+}
+
+function getCurrentLocation() {
+  navigator.geolocation.getCurrentPosition(function(result) {
+    lat = result.coords.latitude; // latitude value
+    long = result.coords.longitude; // longitude value
+  });
 }
