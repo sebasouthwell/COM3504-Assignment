6d718d7088e729880c18eecc5ecf3600228a2509
Joel Hunt <joelmartinhunt@gmail.com>
2024-04-24 18:23:03 +0100

    Added login nickname to header

--- routes/sighting.js
+++ routes/sighting.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 let stylesheets = ["bootstrap/dist/css/bootstrap.min.css","stylesheets/style.css","bootstrap-icons/font/bootstrap-icons.css"];
-let javascript = ["jquery/dist/jquery.js","bootstrap/dist/js/bootstrap.js","bootstrap/dist/js/bootstrap.bundle.js"];
+let javascript = ["jquery/dist/jquery.js","bootstrap/dist/js/bootstrap.js","bootstrap/dist/js/bootstrap.bundle.js","javascripts/indexDBHandler.js","javascripts/nickname.js"];
 router.get('/', function(req, res, next) {
     js = javascript;
     js.push("javascripts/locationManager.js");
@@ -24,7 +24,6 @@ router.get('/view_all', function(req, res, next) {
 
 router.get('/login', function(req, res, next) {
     js = javascript;
-    js.push("javascripts/indexDBHandler.js");
     res.render('login', { title: 'Planttrest: Login',stylesheets: stylesheets, javascripts: js});
 });
 
